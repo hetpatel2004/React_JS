1d6f5be4d71301c410220f48287dfc3e81b9cece
hetpatel2004 <het.patel.010420047@gmail.com>
2026-03-26 21:01:03 +0530

    26-march-2026

---
+++ practice/src/components/UseImerativehandel/Child_one.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Child_one() {
+  return (
+    <div>Child_one</div>
+  )
+}
+
+export default Child_one
---
+++ practice/src/components/UseImerativehandel/Parent_one.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Parent_one() {
+  return (
+    <div>Parent_one</div>
+  )
+}
+
+export default Parent_one
